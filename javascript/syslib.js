df7cd8056f00902a8f60ba28e30e88176d025b13
rjohnsenmw <roger.johnsen@makingwaves.no>
2014-11-25 10:09:07 +0100

    Making Instagram configurable

---
+++ javascript/syslib.js
@@ -0,0 +1,10 @@
+jQuery.noConflict();
+
+/**
+ * Get query parameters
+ */
+jQuery.extend({
+    getQueryParameters : function(str) {
+        return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
+    }  
+});
